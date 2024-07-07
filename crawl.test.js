60f2cc1578e03f387071f6bb38af0c173fa0fbd3
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-07-07 16:38:53 +0530

    added various tests for normalize URL's

---
+++ crawl.test.js
@@ -0,0 +1,32 @@
+const {normalizeURL} = require('./crawl.js');
+const {test, expect} = require('@jest/globals');
+
+test('normalizeURL strip protocol', () => {
+    const input = "https://blog.boot.dev/path";
+    const expected = "blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    expect(actual).toBe(expected);
+});
+
+test('normalizeURL strip trailing slash', () => {
+    const input = "https://blog.boot.dev/path/";
+    const expected = "blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    expect(actual).toBe(expected);
+});
+
+test('normalizeURL capitals', () => {
+    const input = "https://BLOG.boot.dev/path/";
+    const expected = "blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    expect(actual).toBe(expected);
+});
+
+test('normalizeURL strip http', () => {
+    const input = "http://BLOG.boot.dev/path/";
+    const expected = "blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    expect(actual).toBe(expected);
+});
+
+
