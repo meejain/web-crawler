69027ca932a37a711e44e8063dada1f2156842d5
Meet Kumar Jain <meejain@meets-mbp-2.corp.adobe.com>
2024-07-22 20:48:31 +0530

    Fixing bugs based on the output

--- main.js
+++ main.js
@@ -31,41 +31,43 @@ async function main() {
     }
     const baseURL = process.argv[2];
 
+    await crawling(baseURL);
+
     //check for robots.txt / sitemap.txt
-    try {
-        if (baseURL.slice(-1) === '/') {
-            if (!baseURL.includes('//www')) {
-                newBaseURL = baseURL.slice(0, -1).replace('//', '//www.');
-            }
-            else {
-                newBaseURL = baseURL.slice(0, -1);
-            }
-          }
-          else {
-            if (!baseURL.includes('//www')) {
-                newBaseURL = baseURL.replace('//', '//www.');
-            }
-            else {
-                newBaseURL = baseURL
-            }
-          }
-        const robotsURL = new URL('/robots.txt', newBaseURL);
-        console.log(robotsURL);
-        resp = await fetch(robotsURL);
-    } catch (err) {
-        console.log(`Error fetching ${newBaseURL}/robots.txt: ${err.message}`);
-    }
-    if (resp.status === 200) {
-        console.log(`Found robots.txt for ${baseURL}, hence getting URL's from Sitemap`);
-        crawlStatus.urls = await loadURLsFromRobots(newBaseURL, newBaseURL);
-        console.log(crawlStatus.urls);
-        if (crawlStatus.urls.length === 0) {
-            console.log("Issue accessing sitemap, hence crawling the base URL");
-            await crawling(baseURL);
-        }
-    } else {
-        await crawling(baseURL);
-    }
+    // try {
+    //     if (baseURL.slice(-1) === '/') {
+    //         if (!baseURL.includes('//www')) {
+    //             newBaseURL = baseURL.slice(0, -1).replace('//', '//www.');
+    //         }
+    //         else {
+    //             newBaseURL = baseURL.slice(0, -1);
+    //         }
+    //       }
+    //       else {
+    //         if (!baseURL.includes('//www')) {
+    //             newBaseURL = baseURL.replace('//', '//www.');
+    //         }
+    //         else {
+    //             newBaseURL = baseURL
+    //         }
+    //       }
+    //     const robotsURL = new URL('/robots.txt', newBaseURL);
+    //     console.log(robotsURL);
+    //     resp = await fetch(robotsURL);
+    // } catch (err) {
+    //     console.log(`Error fetching ${newBaseURL}/robots.txt: ${err.message}`);
+    // }
+    // if (resp.status === 200) {
+    //     console.log(`Found robots.txt for ${baseURL}, hence getting URL's from Sitemap`);
+    //     crawlStatus.urls = await loadURLsFromRobots(newBaseURL, newBaseURL);
+    //     console.log(crawlStatus.urls);
+    //     if (crawlStatus.urls.length === 0) {
+    //         console.log("Issue accessing sitemap, hence crawling the base URL");
+    //         await crawling(baseURL);
+    //     }
+    // } else {
+    //     await crawling(baseURL);
+    // }
 }
 
 main();
