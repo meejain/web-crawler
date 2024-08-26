50313dfed1f48be4f77515248ac0e3703ceab009
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-08-26 08:20:30 +0530

    Adding the logic for getting the LHS of the sitemap URL's

--- main.js
+++ main.js
@@ -6,7 +6,86 @@ const crawlStatus = {
     crawled: 0,
     rows: [],
     urls: [],
-  };
+};
+
+class inputObj {
+    constructor(Company_Name, Crawled_URL) {
+        this.Company_Name = Company_Name;
+        this.Crawled_URL = Crawled_URL;
+    }
+}
+
+
+  function setUpQueryDesktop(site) {
+    const YOUR_API_KEY = "AIzaSyCwZkCTnraHXOjnCWuq2oxXJOE-ll1hzuI";
+    const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
+    if (!site.startsWith('http')){ site = "https://" + site; }
+    const parameters = {
+        url: encodeURIComponent(site)
+    };
+    let query = `${api}?`;
+    for (let key in parameters) {
+        query += `${key}=${parameters[key]}`;
+    }
+    // Add API key at the end of the query
+    query += `&key=${YOUR_API_KEY}`;
+    return query;
+}
+
+
+
+  function setUpQueryMobile(site) {
+    const YOUR_API_KEY = "AIzaSyCwZkCTnraHXOjnCWuq2oxXJOE-ll1hzuI";
+    const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
+    if (!site.startsWith('http')){ site = "https://" + site; }
+    const parameters = {
+        url: encodeURIComponent(site)
+    };
+    let query = `${api}?`;
+    for (let key in parameters) {
+        query += `${key}=${parameters[key]}`;
+    }
+    // Add API key at the end of the query
+    query += "&strategy=mobile";
+    query += `&key=${YOUR_API_KEY}`;
+    return query;
+}
+
+  async function fetchURL(url) {
+    const resp = await fetch(url);
+    const response = await resp.json();
+    return response;
+ }
+
+
+  async function lhsrun(site, customer) {
+    const terms = [".json", "?", "granite/core", "404.html", "healthcheck", "jpg", "css", "svg", "*"];
+    const result1 = terms.some(term => site.includes(term));
+    if (result1) { console.log(customer + "#" + site + "#" + "We need a different URL"); }
+    else {
+        const conditions = ["Unable to process request"];
+        const urlMobile = setUpQueryMobile(site);
+        const urlDesktop = setUpQueryDesktop(site);
+        const responseMobile = await fetchURL(urlMobile);
+        const responseDesktop = await fetchURL(urlDesktop);
+        // (responseMobile.error) ? ((conditions.some(el => responseMobile.error.message.includes(el))) ? lhsrun(site, customer) : console.log(customer + "#" + site + "#" + " LHS is erroring with " + responseMobile.error.message)) : console.log(customer + "#" + site + "#" + (Math.round(responseMobile.lighthouseResult.categories.performance.score * 100) + "%") + "#" + (Math.round(responseDesktop.lighthouseResult.categories.performance.score * 100) + "%"));
+        const result = (responseMobile.error) ? ((conditions.some(el => responseMobile.error.message.includes(el))) ? lhsrun(site, customer) : (customer + "#" + site + "#" + " LHS is erroring with " + responseMobile.error.message)) : (customer + "#" + site + "#" + (Math.round(responseMobile.lighthouseResult.categories.performance.score * 100)) + "#" + (Math.round(responseDesktop.lighthouseResult.categories.performance.score * 100)));
+        return result;
+    }
+}
+
+  async function displayLHS(data) {
+    const result = await lhsrun(data.Crawled_URL, data.Company_Name);
+    console.log(result);
+  }
+
+  async function mainfunction() {
+    arrayReport = []
+    for (let i = 0; i <= (raw_data.length-1); i++) {
+        if ((!raw_data[i].Company_Name) && (!raw_data[i].Crawled_URL)) { console.log("\n"); continue; }
+        (raw_data[i].Crawled_URL) ? await displayLHS(raw_data[i]) : console.log(raw_data[i].Company_Name+"##No Crawled_URL");
+    }
+}
 
 async function crawling(baseURL) {
     console.log(`Crawling ${baseURL}`);
@@ -62,9 +141,28 @@ async function main() {
             console.log(`Found robots.txt for ${baseURL}, hence getting URL's from Sitemap`);
             crawlStatus.urls = await loadURLsFromRobots(newBaseURL, newBaseURL);
             console.log(crawlStatus.urls);
+
             if (crawlStatus.urls.length === 0) {
                 console.log("Issue accessing sitemap, hence crawling the base URL");
+                return;
             }
+
+            //Code below to get the LHS of the URL's from sitemap
+            raw_data = [];
+            targetUrl = '';
+
+            crawlStatus.urls.slice(0, 5).forEach((url) => {
+                const urlPattern = /^http/;
+                if (!urlPattern.test(url)) {
+                    url = 'https://' + url;
+                }
+          
+                targetUrl = url;
+                inputObject = new inputObj('AMS', url);
+                raw_data.push(inputObject);
+            });
+            console.log("Fetching the Performance Scores for the URL's from sitemap . . . ");
+            mainfunction();
         } else {
             console.log("Issue accessing sitemap, hence crawling the base URL");
         } 
