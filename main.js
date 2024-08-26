5c405d9eae2c519419a61d06b11b30564bcac23a
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-08-26 07:40:35 +0530

    Adding the logic of seggregation of crawling via sitemap or google

--- main.js
+++ main.js
@@ -22,52 +22,55 @@ async function main() {
     var resp = null;
     var newBaseURL = null;
 
-    if (process.argv.length < 3) {
+    if (process.argv.length < 4) {
         console.log("Usage: node main.js <input_file>");
         process.exit(1);
-    } else if (process.argv.length > 3) {
+    } else if (process.argv.length > 4) {
         console.log("Usage is just 1 website: node main.js <input_file>");
         process.exit(1);
     }
     const baseURL = process.argv[2];
 
-    await crawling(baseURL);
+    const ask = process.argv[3];
 
-    //check for robots.txt / sitemap.txt
-    // try {
-    //     if (baseURL.slice(-1) === '/') {
-    //         if (!baseURL.includes('//www')) {
-    //             newBaseURL = baseURL.slice(0, -1).replace('//', '//www.');
-    //         }
-    //         else {
-    //             newBaseURL = baseURL.slice(0, -1);
-    //         }
-    //       }
-    //       else {
-    //         if (!baseURL.includes('//www')) {
-    //             newBaseURL = baseURL.replace('//', '//www.');
-    //         }
-    //         else {
-    //             newBaseURL = baseURL
-    //         }
-    //       }
-    //     const robotsURL = new URL('/robots.txt', newBaseURL);
-    //     console.log(robotsURL);
-    //     resp = await fetch(robotsURL);
-    // } catch (err) {
-    //     console.log(`Error fetching ${newBaseURL}/robots.txt: ${err.message}`);
-    // }
-    // if (resp.status === 200) {
-    //     console.log(`Found robots.txt for ${baseURL}, hence getting URL's from Sitemap`);
-    //     crawlStatus.urls = await loadURLsFromRobots(newBaseURL, newBaseURL);
-    //     console.log(crawlStatus.urls);
-    //     if (crawlStatus.urls.length === 0) {
-    //         console.log("Issue accessing sitemap, hence crawling the base URL");
-    //         await crawling(baseURL);
-    //     }
-    // } else {
-    //     await crawling(baseURL);
-    // }
+    if (ask === "s") {
+        //check for robots.txt / sitemap.txt
+        try {
+            if (baseURL.slice(-1) === '/') {
+                if (!baseURL.includes('//www')) {
+                    newBaseURL = baseURL.slice(0, -1).replace('//', '//www.');
+                }
+                else {
+                    newBaseURL = baseURL.slice(0, -1);
+                }
+              }
+              else {
+                if (!baseURL.includes('//www')) {
+                    newBaseURL = baseURL.replace('//', '//www.');
+                }
+                else {
+                    newBaseURL = baseURL
+                }
+              }
+            const robotsURL = new URL('/robots.txt', newBaseURL);
+            console.log(robotsURL);
+            resp = await fetch(robotsURL);
+        } catch (err) {
+            console.log(`Error fetching ${newBaseURL}/robots.txt: ${err.message}`);
+        }
+        if (resp.status === 200) {
+            console.log(`Found robots.txt for ${baseURL}, hence getting URL's from Sitemap`);
+            crawlStatus.urls = await loadURLsFromRobots(newBaseURL, newBaseURL);
+            console.log(crawlStatus.urls);
+            if (crawlStatus.urls.length === 0) {
+                console.log("Issue accessing sitemap, hence crawling the base URL");
+            }
+        } else {
+            console.log("Issue accessing sitemap, hence crawling the base URL");
+        } 
+    } else {
+        await crawling(baseURL);
+    }  
 }
 
 main();
